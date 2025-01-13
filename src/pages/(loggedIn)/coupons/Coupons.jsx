0ae6a8391330743b6999d76d58e742498e74aec1
rock72427 <165383754+rock72427@users.noreply.github.com>
2025-01-13 11:14:07 +0530

    commit

--- src/pages/(loggedIn)/coupons/Coupons.jsx
+++ src/pages/(loggedIn)/coupons/Coupons.jsx
@@ -1,26 +1,18 @@
-// import React from "react";
-// import CouponsHeader from "./CouponsHeader";
-// import CouponsSection from "./CouponsSection";
-// import CouponsContent from "./CouponsContent";
-// import CouponsDetails from "./CouponsDetails";
-
-// const Coupons = () => {
-//   return (
-//     <div>
-//       <CouponsHeader />
-//       <CouponsSection />
-//       <CouponsContent />
-//       {/* <CouponsDetails /> */}
-//     </div>
-//   );
-// };
-
-// export default Coupons;
-
 import React from "react";
+import CouponsHeader from "./CouponsHeader";
+import CouponsSection from "./CouponsSection";
+import CouponsContent from "./CouponsContent";
+import CouponsDetails from "./CouponsDetails";
 
 const Coupons = () => {
-  return <div>Coupons</div>;
+  return (
+    <div>
+      <CouponsHeader />
+      <CouponsSection />
+      <CouponsContent />
+      {/* <CouponsDetails /> */}
+    </div>
+  );
 };
 
 export default Coupons;
